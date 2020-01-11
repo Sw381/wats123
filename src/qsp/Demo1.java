e228678b20da4d2ad920afdf8b76346c2d013173
Sw381 <59757744+Sw381@users.noreply.github.com>
2020-01-10 23:51:49 -0800

    Update Demo1.java

--- src/qsp/Demo1.java
+++ src/qsp/Demo1.java
@@ -5,6 +5,12 @@ public class Demo1 {
 	public static void main(String[] args) {
 		 
 		System.out.println("Hiiii");
+		
+		System.out.println("Hello");
+		
+		System.out.println("bye");
+		
+		
 
 	}
 
